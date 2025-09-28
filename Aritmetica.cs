8dfeb5142ccf49de1d655eb613a4b3a5da8639e6
Cira <231198714+cirav23formacao-source@users.noreply.github.com>
2025-09-28 21:15:05 +0100

    Acrescentar código para testar

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -13,19 +13,7 @@ namespace Calculos
     internal static class Aritmetica
     {
        
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo=0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
-        /// <summary>
-        /// Operação de soma.
-        /// </summary>
-        /// <returns>Retorna a soma de dois números.</returns>
+       
         public static int Somar(int x, int y) {
             return x + y;
         }
@@ -38,22 +26,6 @@ namespace Calculos
             return x - y;
         }
 
-        /// <summary>
-        /// Operação de converter temperatura.
-        /// </summary>
-        /// <returns>Retorna o resultado da conversão de Celsius/Fahrenheit.</returns>
-        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return ((temperatura - 32) / 1.8000);
-            }
-
-            return -1;
-        }
+          
     }
 }
